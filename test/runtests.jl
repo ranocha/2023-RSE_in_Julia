589b166d7ea7d84c00dc6ab3e33586f5bfae08df
Yann Ebling <yaebling@students.uni-mainz.de>
2023-11-07 15:14:44 +0100

    added Aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
 using Test
 using RSEinJulia
+using Aqua: Aqua
 
 @test_nowarn greet()
+
+@testset "Aqua.jl" begin
+    Aqua.test_all(2023-RSE_in_Julia)
+end
 
 @test add(1, 2) == 3
 @test add(1.0, 2.0) ≈ 3.0
