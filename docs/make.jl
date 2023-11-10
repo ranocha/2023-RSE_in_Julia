0e85d23d42c3dbd843b35633095191f47f75065e
Hendrik Ranocha <mail@ranocha.de>
2023-11-10 09:39:28 +0100

    simple setup of docs

---
+++ docs/make.jl
@@ -0,0 +1,41 @@
+using Documenter
+using Pkg: Pkg
+using RSEinJulia
+
+# Define module-wide setups such that the respective modules are available in doctests
+DocMeta.setdocmeta!(RSEinJulia,
+                    :DocTestSetup, :(using RSEinJulia); recursive = true)
+
+# Copy some files from the top level directory to the docs and modify them
+# as necessary
+open(joinpath(@__DIR__, "src", "license.md"), "w") do io
+    # Point to source license file
+    println(io, """
+    ```@meta
+    EditURL = "https://github.com/ranocha/2023-RSE_in_Julia/blob/main/LICENSE.md"
+    ```
+    """)
+    # Write the modified contents
+    println(io, "# License")
+    println(io, "")
+    for line in eachline(joinpath(dirname(@__DIR__), "LICENSE"))
+        line = replace(line, "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
+        println(io, "> ", line)
+    end
+end
+
+# Make documentation
+makedocs(modules = [RSEinJulia],
+         sitename = "2023-RSE_in_Julia",
+         format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",
+                                  canonical = "https://ranocha.github.io/2023-RSE_in_Julia/stable"),
+         # Explicitly specify documentation structure
+         pages = [
+             "Home" => "index.md",
+             "API reference" => "api_reference.md",
+             "License" => "license.md",
+         ])
+
+deploydocs(repo = "github.com/ranocha/2023-RSE_in_Julia",
+           devbranch = "main",
+           push_preview = true)
---
+++ docs/src/index.md
@@ -0,0 +1,12 @@
+# 2023-RSE_in_Julia
+
+Repository supporting the course "Research Software Engineering in Julia"
+at Johannes Gutenberg University Mainz in the winter term 2023/2024.
+This repository is just used as a demonstration how to contribute to
+repositories on GitHub. The teaching material will be distributed via
+the Moodle platform of JGU Mainz.
+
+
+## License
+
+This project is licensed under the MIT license (see [License](@ref)).
