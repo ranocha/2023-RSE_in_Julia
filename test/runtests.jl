487d6c4a2c1c0b51bcac6f9b041dc3333c5d6395
HiIamYann <126207689+HiIamYann@users.noreply.github.com>
2023-11-10 10:20:21 +0100

    added Aqua tests (#12)
    * added Aqua tests
    
    * Update test/runtests.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update test/runtests.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
 using Test
 using RSEinJulia
+using Aqua: Aqua
 
 @test_nowarn greet()
+
+@testset "Aqua.jl" begin
+    # We do not test `project_extras` since we do not follow the established
+    # practice to have the same name of the repository as the Julia package
+    # defined therein.
+    Aqua.test_all(RSEinJulia; project_extras = false)
+end
 
 @test add(1, 2) == 3
 @test add(1.0, 2.0) ≈ 3.0
